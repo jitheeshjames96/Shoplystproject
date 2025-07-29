2a66873993ab04bea4286a1a2b8c36fda1fbb2b5
jitheeshjames96 <jitheeshjames27@gmail.com>
2025-07-29 16:56:20 +0530

    Update user-data-script.sh
    Signed-off-by: jitheeshjames96 <jitheeshjames27@gmail.com>

--- Project/scripts/user-data-script.sh
+++ Project/scripts/user-data-script.sh
@@ -9,33 +9,3 @@ aws s3 cp s3://codebuildjitheesh/Project/scripts/index.html /usr/share/nginx/htm
 systemctl status nginx > /var/log/nginx-status.log
 
 systemctl restart nginx
-
-
-# Update and install required tools
-dnf update -y
-sudo dnf install -y mariadb105
-
-# Input Parameters
-SECRET_ARN=$1
-RDS_ENDPOINT=$2
-
-# Fetch secret values from AWS Secrets Manager
-SECRET_JSON=$(aws secretsmanager get-secret-value --secret-id "$SECRET_ARN" --query SecretString --output text)
-DBUSER=$(echo "$SECRET_JSON" | jq -r .username)
-DBPASS=$(echo "$SECRET_JSON" | jq -r .password)
-
-# Create schema and data
-mysql -h "$RDS_ENDPOINT" -u"$DBUSER" -p"$DBPASS" <<EOF
-CREATE DATABASE IF NOT EXISTS shopalystdb;
-USE shopalystdb;
-CREATE TABLE IF NOT EXISTS products (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(100),
-  price DECIMAL(10,2),
-  category VARCHAR(50)
-);
-INSERT INTO products (name, price, category) VALUES
-  ('Apple iPhone 15', 799.99, 'Electronics'),
-  ('Samsung Galaxy S24', 749.00, 'Electronics'),
-  ('MacBook Air M3', 1299.50, 'Computers');
-EOF
