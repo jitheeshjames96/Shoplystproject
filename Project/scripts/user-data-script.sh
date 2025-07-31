36d8c48902de078d138f8205eeb6c34eec407e6c
jitheeshjames96 <jitheeshjames27@gmail.com>
2025-07-31 14:54:51 +0530

    Update user-data-script.sh
    Signed-off-by: jitheeshjames96 <jitheeshjames27@gmail.com>

--- Project/scripts/user-data-script.sh
+++ Project/scripts/user-data-script.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-echo "Started UserData script" > /tmp/userdata-status.txt
-dnf install -y nginx && echo "Nginx installed" >> /tmp/userdata-status.txt
-systemctl start nginx && echo "Nginx started" >> /tmp/userdata-status.txt
-systemctl enable nginx && echo "Nginx enabled" >> /tmp/userdata-status.txt
+echo "Started UserData script"
+dnf install -y nginx && echo "Nginx installed"
+systemctl start nginx && echo "Nginx started"
+systemctl enable nginx && echo "Nginx enabled"
 
-aws s3 cp s3://codebuildjitheesh/Project/scripts/index.html /usr/share/nginx/html/index.html && echo "Index.html file updated to nginx location" >> /tmp/userdata-status.txt
+aws s3 cp s3://codebuildjitheesh/Project/scripts/index.html /usr/share/nginx/html/index.html && echo "Index.html file updated to nginx location"
 
 systemctl status nginx > /var/log/nginx-status.log
 
 systemctl restart nginx
+
+echo "Restarted nginx"
 
 : <<'COMMENT'
 # Update and install required tools for AL2023
