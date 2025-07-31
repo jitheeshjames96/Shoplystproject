71484e5387568817803334945855af1f788b7376
jitheeshjames96 <jitheeshjames27@gmail.com>
2025-07-31 14:52:51 +0530

    Update bootstrap.sh
    Signed-off-by: jitheeshjames96 <jitheeshjames27@gmail.com>

--- Project/scripts/bootstrap.sh
+++ Project/scripts/bootstrap.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 # Update and install required tools for AL2023
-echo "Started DB script" > /tmp/rds-bootstrap-status.txt
-dnf update -y && echo "Update triggered" >> /tmp/rds-bootstrap-status.txt 
-sudo dnf install -y mariadb105 && echo "Mysql Installed" >> /tmp/rds-bootstrap-status.txt
+echo "Started DB script"
+dnf update -y && echo "Update triggered"
+sudo dnf install -y mariadb105 && echo "Mysql Installed"
 
 # Input Parameters
 DBSecretARN=$1
 DBEndpoint=$2
 
-echo "Secret Arn : $DBSecretARN" > /tmp/rds-bootstrap-status.txt
-echo "DBEndpoint : $DBEndpoint" > /tmp/rds-bootstrap-status.txt
+echo "Secret Arn : $DBSecretARN"
+echo "DBEndpoint : $DBEndpoint"
 
 # Fetch secret values from AWS Secrets Manager
 SECRET_JSON=$(aws secretsmanager get-secret-value --secret-id "$DBSecretARN" --query SecretString --output text)
 DBUSER=$(echo "$SECRET_JSON" | jq -r .username) 
 DBPASS=$(echo "$SECRET_JSON" | jq -r .password)
 
-echo "User : $DBUSER" > /tmp/rds-bootstrap-status.txt
-echo "Password  : $DBPASS" > /tmp/rds-bootstrap-status.txt
-echo "Fetched secret : $SECRET_JSON" > /tmp/rds-bootstrap-status.txt
+echo "User : $DBUSER"
+echo "Password  : $DBPASS"
+echo "Fetched secret : $SECRET_JSON"
 # Create schema and data
 mysql -h "$DBEndpoint" -u"$DBUSER" -p"$DBPASS"
-echo "MySQL Connected" > /tmp/rds-bootstrap-status.txt
+echo "MySQL Connected"
